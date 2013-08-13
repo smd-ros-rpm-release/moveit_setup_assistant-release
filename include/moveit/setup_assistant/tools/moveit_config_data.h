b466d36dadca312a446617295669d7d4322381ea
isucan <isucan@gmail.com>
2013-08-13 19:59:22 +0300

    Rebase from 'upstream'

--- include/moveit/setup_assistant/tools/moveit_config_data.h
+++ include/moveit/setup_assistant/tools/moveit_config_data.h
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -76,14 +76,12 @@ struct GroupMetaData
   int kinematics_solver_attempts_; // solver attempts
 };
 
-// ******************************************************************************************
-// Forward Declarations
-// ******************************************************************************************
-//class urdf::Model;
+/** \brief This class is shared with all widgets and contains the common configuration data
+    needed for generating each robot's MoveIt configuration package.
 
-// ******************************************************************************************
-// Class
-// ******************************************************************************************
+    All SRDF data is contained in a subclass of this class -
+    srdf_writer.cpp. This class also contains the functions for writing
+    out the configuration files. */
 class MoveItConfigData
 {
 public:
