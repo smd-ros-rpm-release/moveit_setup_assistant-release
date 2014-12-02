5cf5fddb4c94198f54ecbe9854302c0104e8ebcc
Dave Coleman <davetcoleman@gmail.com>
2014-12-01 18:42:59 -0700

    Rebase from 'upstream'

--- src/tools/moveit_config_data.cpp
+++ src/tools/moveit_config_data.cpp
@@ -176,14 +176,14 @@ MoveItConfigData::~MoveItConfigData()
 // ******************************************************************************************
 robot_model::RobotModelConstPtr MoveItConfigData::getRobotModel()
 {
-  if( !kin_model_ )
+  if( !robot_model_ )
   {
     // Initialize with a URDF Model Interface and a SRDF Model
-    kin_model_.reset( new robot_model::RobotModel( urdf_model_, srdf_->srdf_model_ ) );
-    kin_model_const_ = kin_model_;
+    robot_model_.reset( new robot_model::RobotModel( urdf_model_, srdf_->srdf_model_ ) );
+    robot_model_const_ = robot_model_;
   }
 
-  return kin_model_const_;
+  return robot_model_const_;
 }
 
 // ******************************************************************************************
@@ -197,8 +197,8 @@ void MoveItConfigData::updateRobotModel()
   srdf_->updateSRDFModel( *urdf_model_ );
 
   // Create new kin model
-  kin_model_.reset( new robot_model::RobotModel( urdf_model_, srdf_->srdf_model_ ) );
-  kin_model_const_ = kin_model_;
+  robot_model_.reset( new robot_model::RobotModel( urdf_model_, srdf_->srdf_model_ ) );
+  robot_model_const_ = robot_model_;
 
   // Reset the planning scene
   planning_scene_.reset();
@@ -215,7 +215,7 @@ planning_scene::PlanningScenePtr MoveItConfigData::getPlanningScene()
     getRobotModel();
 
     // Allocate an empty planning scene
-    planning_scene_.reset(new planning_scene::PlanningScene(kin_model_));
+    planning_scene_.reset(new planning_scene::PlanningScene(robot_model_));
   }
   return planning_scene_;
 }
@@ -296,18 +296,88 @@ bool MoveItConfigData::outputOMPLPlanningYAML( const std::string& file_path )
 
   emitter << YAML::Value << YAML::BeginMap;
 
-  // Add Planners
-  static const std::string planners[] =
-    {"SBL", "EST", "LBKPIECE", "BKPIECE", "KPIECE", "RRT", "RRTConnect", "RRTstar", "TRRT", "PRM", "PRMstar" };
+  std::vector<OMPLPlannerDescription> planner_des;
 
+  OMPLPlannerDescription SBL("SBL","geometric");
+  SBL.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  planner_des.push_back(SBL);
+
+  OMPLPlannerDescription EST("EST", "geometric");
+  EST.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0 setup()" );
+  EST.addParameter("goal_bias", "0.05", "When close to goal select goal, with this probability. default: 0.05");
+  planner_des.push_back(EST);
+
+  OMPLPlannerDescription LBKPIECE("LBKPIECE", "geometric");
+  LBKPIECE.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  LBKPIECE.addParameter("border_fraction", "0.9", "Fraction of time focused on boarder default: 0.9");
+  LBKPIECE.addParameter("min_valid_path_fraction", "0.5", "Accept partially valid moves above fraction. default: 0.5");
+  planner_des.push_back(LBKPIECE);
+
+  OMPLPlannerDescription BKPIECE("BKPIECE", "geometric");
+  BKPIECE.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  BKPIECE.addParameter("border_fraction", "0.9", "Fraction of time focused on boarder default: 0.9");
+  BKPIECE.addParameter("failed_expansion_score_factor", "0.5", "When extending motion fails, scale score by factor. default: 0.5");
+  BKPIECE.addParameter("min_valid_path_fraction", "0.5", "Accept partially valid moves above fraction. default: 0.5");
+  planner_des.push_back(BKPIECE);
+
+  OMPLPlannerDescription KPIECE("KPIECE", "geometric");
+  KPIECE.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  KPIECE.addParameter("goal_bias", "0.05", "When close to goal select goal, with this probability. default: 0.05 ");
+  KPIECE.addParameter("border_fraction", "0.9", "Fraction of time focused on boarder default: 0.9 (0.0,1.]");
+  KPIECE.addParameter("failed_expansion_score_factor", "0.5", "When extending motion fails, scale score by factor. default: 0.5");
+  KPIECE.addParameter("min_valid_path_fraction", "0.5", "Accept partially valid moves above fraction. default: 0.5");
+  planner_des.push_back(KPIECE);
+
+  OMPLPlannerDescription RRT("RRT", "geometric");
+  RRT.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  RRT.addParameter("goal_bias", "0.05", "When close to goal select goal, with this probability? default: 0.05");
+  planner_des.push_back(RRT);
+
+  OMPLPlannerDescription RRTConnect("RRTConnect", "geometric");
+  RRTConnect.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  planner_des.push_back(RRTConnect);
+
+  OMPLPlannerDescription RRTstar("RRTstar", "geometric");
+  RRTstar.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  RRTstar.addParameter("goal_bias", "0.05", "When close to goal select goal, with this probability? default: 0.05");
+  RRTstar.addParameter("delay_collision_checking", "1", "Stop collision checking as soon as C-free parent found. default 1");
+  planner_des.push_back(RRTstar);
+
+  OMPLPlannerDescription TRRT("TRRT", "geometric");
+  TRRT.addParameter("range", "0.0", "Max motion added to tree. ==> maxDistance_ default: 0.0, if 0.0, set on setup()");
+  TRRT.addParameter("goal_bias", "0.05", "When close to goal select goal, with this probability? default: 0.05");
+  TRRT.addParameter("max_states_failed", "10", "when to start increasing temp. default: 10");
+  TRRT.addParameter("temp_change_factor", "2.0", "how much to increase or decrease temp. default: 2.0");
+  TRRT.addParameter("min_temperature", "10e-10", "lower limit of temp change. default: 10e-10");
+  TRRT.addParameter("init_temperature", "10e-6", "initial temperature. default: 10e-6");
+  TRRT.addParameter("frountier_threshold", "0.0", "dist new state to nearest neighbor to disqualify as frontier. default: 0.0 set in setup() ");
+  TRRT.addParameter("frountierNodeRatio", "0.1", "1/10, or 1 nonfrontier for every 10 frontier. default: 0.1");
+  TRRT.addParameter("k_constant", "0.0", "value used to normalize expresssion. default: 0.0 set in setup()");
+  planner_des.push_back(TRRT);
+
+  OMPLPlannerDescription PRM("PRM", "geometric");
+  PRM.addParameter("max_nearest_neighbors", "10", "use k nearest neighbors. default: 10");
+  planner_des.push_back(PRM);
+
+  OMPLPlannerDescription PRMstar("PRMstar", "geometric"); // no delcares in code
+  planner_des.push_back(PRMstar);
+
+  // Add Planners with parameter values 
   std::vector<std::string> pconfigs;
-  for (std::size_t i = 0 ; i < sizeof(planners) / sizeof(std::string) ; ++i)
+  for (std::size_t i = 0 ; i < planner_des.size() ; ++i)
   {
-    std::string defaultconfig = planners[i] + "kConfigDefault";
+    std::string defaultconfig = planner_des[i].name_ + "kConfigDefault";
     emitter << YAML::Key << defaultconfig;
     emitter << YAML::Value << YAML::BeginMap;
-    emitter << YAML::Key << "type" << YAML::Value << "geometric::" + planners[i];
+    emitter << YAML::Key << "type" << YAML::Value << "geometric::" + planner_des[i].name_;
+    for(std::size_t  j=0; j<planner_des[i].parameter_list_.size(); j++) 
+      {
+	emitter << YAML::Key  << planner_des[i].parameter_list_[j].name;
+	emitter << YAML::Value << planner_des[i].parameter_list_[j].value;
+	emitter << YAML::Comment(planner_des[i].parameter_list_[j].comment.c_str());
+      }
     emitter << YAML::EndMap;
+
     pconfigs.push_back(defaultconfig);
   }
 
@@ -534,7 +604,10 @@ bool MoveItConfigData::outputJointLimitsYAML( const std::string& file_path )
     ROS_ERROR_STREAM( "Unable to open file for writing " << file_path );
     return false;
   }
-
+  // Add documentation into joint_limits.yaml
+  output_stream << "# joint_limits.yaml allows the dynamics properties specified in the URDF to be overwritten or augmented as needed" << std::endl;
+  output_stream << "# Specific joint properties can be changed with the keys [max_position, min_position, max_velocity, max_acceleration]" << std::endl;
+  output_stream << "# Joint limits can be turned off with [has_velocity_limits, has_acceleration_limits]" << std::endl;
   output_stream << emitter.c_str();
   output_stream.close();
 
@@ -596,22 +669,38 @@ bool MoveItConfigData::inputKinematicsYAML( const std::string& file_path )
     yaml_node_t prop_name;
 
     // Loop through all groups
+#ifdef HAVE_NEW_YAMLCPP
+    for( YAML::const_iterator group_it = doc.begin(); group_it != doc.end(); ++group_it )
+#else
     for( YAML::Iterator group_it = doc.begin(); group_it != doc.end(); ++group_it )
+#endif
     {
+#ifdef HAVE_NEW_YAMLCPP
+      const std::string group_name = group_it->first.as<std::string>();
+#else
       std::string group_name;
       group_it.first() >> group_name;
+#endif
 
       // Create new meta data
       GroupMetaData new_meta_data;
 
       // kinematics_solver
+#ifdef HAVE_NEW_YAMLCPP
+      if( prop_name = findValue( group_it->second, "kinematics_solver" ) )
+#else
       if( prop_name = findValue( group_it.second(), "kinematics_solver" ) )
+#endif
       {
         *prop_name >> new_meta_data.kinematics_solver_;
       }
 
       // kinematics_solver_search_resolution
+#ifdef HAVE_NEW_YAMLCPP
+      if( prop_name = findValue( group_it->second, "kinematics_solver_search_resolution" ) )
+#else
       if( prop_name = findValue( group_it.second(), "kinematics_solver_search_resolution" ) )
+#endif
       {
         *prop_name >> new_meta_data.kinematics_solver_search_resolution_;
       }
@@ -621,7 +710,11 @@ bool MoveItConfigData::inputKinematicsYAML( const std::string& file_path )
       }
 
       // kinematics_solver_timeout
+#ifdef HAVE_NEW_YAMLCPP
+      if( prop_name = findValue( group_it->second, "kinematics_solver_timeout" ) )
+#else
       if( prop_name = findValue( group_it.second(), "kinematics_solver_timeout" ) )
+#endif
       {
         *prop_name >> new_meta_data.kinematics_solver_timeout_;
       }
@@ -631,7 +724,11 @@ bool MoveItConfigData::inputKinematicsYAML( const std::string& file_path )
       }
 
       // kinematics_solver_attempts
+#ifdef HAVE_NEW_YAMLCPP
+      if( prop_name = findValue( group_it->second, "kinematics_solver_attempts" ) )
+#else
       if( prop_name = findValue( group_it.second(), "kinematics_solver_attempts" ) )
+#endif
       {
         *prop_name >> new_meta_data.kinematics_solver_attempts_;
       }
